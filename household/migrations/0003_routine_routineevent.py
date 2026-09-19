c411300472bf501a745ab312a4bc74742f839e66
Mykola D <130664123+MykolaDotsenko@users.noreply.github.com>
2026-09-19 13:04:20 +0300

    Align Routine monthly anchor migration constraint

--- household/migrations/0003_routine_routineevent.py
+++ household/migrations/0003_routine_routineevent.py
@@ -97,6 +97,7 @@ class Migration(migrations.Migration):
                         condition=(
                             models.Q(
                                 frequency="monthly",
+                                recurrence_anchor_day__isnull=False,
                                 recurrence_anchor_day__gte=1,
                                 recurrence_anchor_day__lte=31,
                             )
