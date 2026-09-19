1e98fcc85641a50f1680e56d1d507715ee518fc9
Mykola D <130664123+MykolaDotsenko@users.noreply.github.com>
2026-09-19 11:39:39 +0300

    Keep StreamField metadata migration state-only

--- blog/migrations/0002_alter_blogpage_body.py
+++ blog/migrations/0002_alter_blogpage_body.py
@@ -8,35 +8,40 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name="blogpage",
-            name="body",
-            field=wagtail.fields.StreamField(
-                [
-                    ("heading", 0),
-                    ("paragraph", 1),
-                    ("image", 2),
-                    ("quote", 3),
-                    ("embed", 4),
-                ],
-                block_lookup={
-                    0: (
-                        "wagtail.blocks.CharBlock",
-                        (),
-                        {
-                            "form_classname": "full title",
-                            "template": "blog/streamfield/blocks/heading_block.html",
+        migrations.SeparateDatabaseAndState(
+            database_operations=[],
+            state_operations=[
+                migrations.AlterField(
+                    model_name="blogpage",
+                    name="body",
+                    field=wagtail.fields.StreamField(
+                        [
+                            ("heading", 0),
+                            ("paragraph", 1),
+                            ("image", 2),
+                            ("quote", 3),
+                            ("embed", 4),
+                        ],
+                        block_lookup={
+                            0: (
+                                "wagtail.blocks.CharBlock",
+                                (),
+                                {
+                                    "form_classname": "full title",
+                                    "template": "blog/streamfield/blocks/heading_block.html",
+                                },
+                            ),
+                            1: ("wagtail.blocks.RichTextBlock", (), {}),
+                            2: (
+                                "wagtail.images.blocks.ImageChooserBlock",
+                                (),
+                                {"template": "blog/streamfield/blocks/image_block.html"},
+                            ),
+                            3: ("wagtail.blocks.BlockQuoteBlock", (), {}),
+                            4: ("wagtail.embeds.blocks.EmbedBlock", (), {}),
                         },
                     ),
-                    1: ("wagtail.blocks.RichTextBlock", (), {}),
-                    2: (
-                        "wagtail.images.blocks.ImageChooserBlock",
-                        (),
-                        {"template": "blog/streamfield/blocks/image_block.html"},
-                    ),
-                    3: ("wagtail.blocks.BlockQuoteBlock", (), {}),
-                    4: ("wagtail.embeds.blocks.EmbedBlock", (), {}),
-                },
-            ),
+                )
+            ],
         ),
     ]
