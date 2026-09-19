b544160fa24da88f5f0280adc522b8e3aec71a90
Mykola D <130664123+MykolaDotsenko@users.noreply.github.com>
2026-09-19 11:53:02 +0300

    Add Django authentication routes to app shell

--- mysite/urls.py
+++ mysite/urls.py
@@ -10,6 +10,7 @@ from search import views as search_views
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
 ]
