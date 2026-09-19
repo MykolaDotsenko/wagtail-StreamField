9e695126296da3ad4ff0465e39f0bf793777f76d
Mykola D <130664123+MykolaDotsenko@users.noreply.github.com>
2026-09-19 12:07:45 +0300

    Test database shopping lifecycle constraints

--- household/tests/test_models.py
+++ household/tests/test_models.py
@@ -28,14 +28,32 @@ class ShoppingItemModelTests(TestCase):
             ShoppingItem.objects.create(user=self.user, name="  MILK  ")
 
     def test_same_identity_can_exist_as_completed_and_open(self):
+        from django.utils import timezone
+
         completed = ShoppingItem.objects.create(
             user=self.user,
             name="Milk",
             status=ShoppingItem.Status.COMPLETED,
+            completed_at=timezone.now(),
         )
         open_item = ShoppingItem.objects.create(user=self.user, name="milk")
 
         self.assertNotEqual(completed.pk, open_item.pk)
+
+    def test_empty_normalized_identity_is_rejected(self):
+        with self.assertRaises(IntegrityError), transaction.atomic():
+            ShoppingItem.objects.create(
+                user=self.user,
+                name="   ",
+            )
+
+    def test_completed_state_requires_completed_timestamp(self):
+        with self.assertRaises(IntegrityError), transaction.atomic():
+            ShoppingItem.objects.create(
+                user=self.user,
+                name="Bread",
+                status=ShoppingItem.Status.COMPLETED,
+            )
 
     def test_quantity_must_be_at_least_one(self):
         with self.assertRaises(IntegrityError), transaction.atomic():
